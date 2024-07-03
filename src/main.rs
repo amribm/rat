f18cc6ba3b9f6c3a9cc86b948e0e08da63b2719b
Ameer Ibrahim <ameer@Ameers-MacBook-Pro.local>
2024-07-03 06:02:15 +0530

    add(core): simple implemetation of cat

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::io::Result;
+
+fn main() -> Result<()> {
+    let file = fs::read_to_string("README.md")?;
+
+    print!("{file}");
+
+    Ok(())
 }
