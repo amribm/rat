58219215937e9ebeb9a85b68b9f1cc86ee1f46e6
Ameer Ibrahim <ameer@Ameers-MacBook-Pro.local>
2024-07-03 21:38:10 +0530

    feat(user input): change for accepting file names from user

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,40 @@
-use std::fs;
-use std::io::Result;
+use std::io::{self, Result};
+use std::{env, fs};
+
+use clap::{Arg, ArgAction, Command};
+
+mod options {
+    pub const FILES: &str = "files";
+}
 
 fn main() -> Result<()> {
-    let file = fs::read_to_string("README.md")?;
+    let matches = app().get_matches_from(env::args_os());
 
-    print!("{file}");
+    let files: Vec<String> = match matches.get_many::<String>(options::FILES) {
+        Some(s) => s.map(|x| x.to_string()).collect(),
+        None => Vec::new(),
+    };
+
+    for file in files {
+        print_file_lines(file)?;
+    }
+
+    Ok(())
+}
+
+fn app() -> Command {
+    Command::new("rat")
+        .bin_name("/target/debug/rat")
+        .about("simple `cat` like command line uitility")
+        .arg(Arg::new(options::FILES).action(ArgAction::Append))
+}
+
+fn print_file_lines(file: String) -> io::Result<()> {
+    let file_stirng = fs::read_to_string(file)?;
+
+    for line in file_stirng.lines() {
+        println!("{line}");
+    }
 
     Ok(())
 }
