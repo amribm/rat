c28e708b0dbaca7ae60aab18d2052a8efc9dae6b
i-ai-dev <ameerthecoder@gmail.com>
2024-07-06 17:03:55 +0530

    feat(print); print with $ at the end and print fast ingnoring other flags

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::io::{self, Result};
-use std::{env, fmt, fs};
+use std::{env, fs};
 
 use clap::{Arg, ArgAction, Command};
 
@@ -7,25 +7,47 @@ mod options {
     pub const FILES: &str = "files";
     pub const NUMBER_NON_BLANK: &str = "number non blank lines";
     pub const NUMBER_LINES: &str = "number lines";
+    pub const ADD_END_DOLLER: &str = "add `$` at the end of line";
+    pub const FAST_PRINT: &str = "fast print";
+}
+
+struct Flags {
+    number_lines: bool,
+    number_non_blank_lines: bool,
+    add_dollar_end: bool,
+    fast_print: bool,
+}
+
+impl Flags {
+    fn new() -> Flags {
+        Flags {
+            number_lines: false,
+            number_non_blank_lines: false,
+            add_dollar_end: false,
+            fast_print: false,
+        }
+    }
 }
 
 fn main() -> Result<()> {
+    let mut flags = Flags::new();
     let matches = app().get_matches_from(env::args_os());
 
-    let number_non_blank_lines = matches.get_flag(options::NUMBER_NON_BLANK);
-    let mut number_lines = matches.get_flag(options::NUMBER_LINES);
+    flags.number_non_blank_lines = matches.get_flag(options::NUMBER_NON_BLANK);
+    flags.number_lines = matches.get_flag(options::NUMBER_LINES);
+    flags.add_dollar_end = matches.get_flag(options::ADD_END_DOLLER);
 
     let files: Vec<String> = match matches.get_many::<String>(options::FILES) {
         Some(s) => s.map(|x| x.to_string()).collect(),
         None => Vec::new(),
     };
 
-    if number_non_blank_lines {
-        number_lines = true;
+    if flags.number_non_blank_lines {
+        flags.number_lines = true;
     }
 
     for file in files {
-        print_file_lines(file, number_lines, number_non_blank_lines)?;
+        print_file_lines(file, &flags)?;
     }
 
     Ok(())
@@ -49,27 +71,51 @@ fn app() -> Command {
                 .action(ArgAction::SetTrue)
                 .overrides_with(options::NUMBER_NON_BLANK),
         )
+        .arg(
+            Arg::new(options::ADD_END_DOLLER)
+                .short('e')
+                .help("add `$` at the end of the line")
+                .action(ArgAction::SetTrue)
+                .overrides_with(options::ADD_END_DOLLER),
+        )
+        .arg(
+            Arg::new(options::FAST_PRINT)
+                .short('f')
+                .help("prints fast")
+                .action(ArgAction::SetTrue)
+                .overrides_with(options::FAST_PRINT),
+        )
         .arg(Arg::new(options::FILES).action(ArgAction::Append))
 }
 
-fn print_file_lines(file: String, number_lines: bool, non_blank_lines: bool) -> io::Result<()> {
+fn print_file_lines(file: String, flags: &Flags) -> io::Result<()> {
     let file_stirng = fs::read_to_string(file)?;
+    if flags.fast_print {
+        print!("{file_stirng}")
+    }
 
     let mut count = 0;
     for line in file_stirng.lines() {
         let mut line = format!("{line}");
-        if number_lines {
-            if line.is_empty() {
-                if non_blank_lines {
-                    println!("");
-                    continue;
-                }
-            }
-            count += 1;
-            line = format!("{:>6}  {line}", count);
+        if flags.number_lines {
+            line = number_line(&line, flags.number_non_blank_lines, &mut count);
+        }
+
+        if flags.add_dollar_end {
+            line.push('$');
         }
         println!("{line}");
     }
 
     Ok(())
+}
+
+fn number_line(line: &str, number_non_blank_lines: bool, count: &mut i32) -> String {
+    if line.is_empty() {
+        if number_non_blank_lines {
+            return format!("{:>6}  {line}", "");
+        }
+    }
+    *count += 1;
+    format!("{:>6}  {line}", count)
 }
